b77c632e0f48f76961c50c21b3fcb48f45d337b2
hunterxcobby <solomonsefah13@gmail.com>
2024-08-11 17:56:38 +0000

    building tic-tac-toe game

--- 0-tic-tac-toe/main.py
+++ 0-tic-tac-toe/main.py
@@ -9,8 +9,16 @@ creae a class to show our board
 
 class Board():
     def __init__ (self): 
-        self.cells = [" ","X ","O","X"," "," "," "," "," "]
+        self.cells = [" "," "," "," "," "," "," "," "," "," "]
     
     """a function called display to display the board"""
-    def function(self):
-        print (" %s | %s | %s " %(self.cells[1], self.cells[2], self.cells[3]))
+    def display(self):
+        print(" %s | %s | %s " %(self.cells[1], self.cells[2], self.cells[3]))
+        print("-----------")
+        print(" %s | %s | %s " %(self.cells[4], self.cells[5], self.cells[6]))
+        print("-----------")
+        print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
+
+
+board = Board()
+board.display()
