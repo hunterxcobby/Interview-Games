c28eb3c905b4535f4006a9a54feabf7c8bc57a80
hunterxcobby <solomonsefah13@gmail.com>
2024-08-21 14:28:44 +0000

    chore: Update snake game settings

--- 2-snake_game/main.py
+++ 2-snake_game/main.py
@@ -3,6 +3,15 @@
 from tkinter import *
 import random
 
+# GAME SETTINGS
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 2
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
 class Snake():
     pass
