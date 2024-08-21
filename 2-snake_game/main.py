dba776cb953833ad34fce126402dd2b2b8755322
hunterxcobby <solomonsefah13@gmail.com>
2024-08-21 14:53:37 +0000

    Refactor snake game logic for improved game flow and collision detection

--- 2-snake_game/main.py
+++ 2-snake_game/main.py
@@ -9,4 +9,13 @@ wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
+# snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
 wn.mainloop()
