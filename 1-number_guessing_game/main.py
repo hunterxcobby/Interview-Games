56212389983ffc2be9d71f3a6e46c7fa4653525f
hunterxcobby <solomonsefah13@gmail.com>
2024-08-21 14:02:25 +0000

    Refactor number guessing game logic for input validation and game flow

--- 1-number_guessing_game/main.py
+++ 1-number_guessing_game/main.py
@@ -10,3 +10,27 @@ is_running = True
 
 print("Number Guessing Game")
 print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+
+    guess = input("Input your guess:\n")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("Your value is out of range")
+            print(f"Please select a number in the range of {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low, please try again")
+        elif guess > answer:
+            print("Too high, please try again")
+        else:
+            print(f"CORRECT !  The answer is {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+
+    else:
+        print("Invalid value")
+        print(f"Select a number between {lowest_num} and {highest_num}")
