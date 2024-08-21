3f2e47447893c2a17a3147c069befed75d34a7bc
hunterxcobby <solomonsefah13@gmail.com>
2024-08-21 14:24:21 +0000

    Refactor snake game logic for game flow and collision detection

---
+++ 2-snake_game/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from tkinter import *
+import random
+
+
+class Snake():
+    pass
+
+class Food():
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
