17dc8538ce2358d54cced7198b42e509174ba909
hunterxcobby <solomonsefah13@gmail.com>
2024-08-22 16:53:04 +0000

    Refactor quiz game logic to handle questions and improve user interface

---
+++ 4-guizgame/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("##########################")
+print("Hello, Welcome To My Quiz Game")
+
+
+question_bank = [
+    {"text": "The abilily of one class to acquire methods and attributes from another class is called ___.","answer": "A"},
+    {"text": "Which of the following is a type of in inheritance?","answer": "D"},
+    {"text": "What type of inherintance has multiple subclasess inheriting from a single base class?","answer": "C"},
+    {"text": "What is the depth of multilevel inheritance?","answer": "C"},
+    {"text:": "What does MRO stand for","answer": "B"},
+]
+
