d6b10d3b827d6cfc0cbdcc8ff0b4d5a75481924c
hunterxcobby <solomonsefah13@gmail.com>
2024-08-22 14:14:12 +0000

    Refactor hangman game logic to add hangman drawing functionality

---
+++ 3-Hangman/main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import random
+from words import word_list
+
+word = random.choice(word_list)
+
+for x in word:
+    print("_", end=" ")
+
+
+#  we need a function to print the hangman 
+def print_hangman(wrong):
+    if(wrong == 0):
+        print("\n+---+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 1):
+        print("\n+---+")
+        print("O   |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 2):
+        print("\n+---+")
+        print("O   |")
+        print("|   |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 3):
+        print("\n+---+")
+        print(" O  |")
+        print("/|  |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 4):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 5):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("/   |")
+        print("   ===")
+    elif(wrong == 6):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("/ \ |")
+        print("   ===")   
+    
+    
+def print_word(guessed_letters):
+    counter = 0
+    rightletters= 0
+
+    for char in word_list:
+        if char in guessed_letters():
