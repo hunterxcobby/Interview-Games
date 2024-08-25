8668aae3485f719966298f82e50dc36a6c97ebca
hunterxcobby <solomonsefah13@gmail.com>
2024-08-25 08:07:46 +0000

    chore: Refactor rock-paper-scissors game logic and user input handling

--- 5-rock_paper_scissors/main.py
+++ 5-rock_paper_scissors/main.py
@@ -3,17 +3,21 @@
 import random
 
 user_choice = int(input("Enter your choice. 0 for Rock, 1 for Paper, 2 for scissors: \n"))
-cmp_choice = random.randint(0,2)
-print(cmp_choice)
 
-# Print the conditions in this particular order to ensure game works fine 
-if cmp_choice == user_choice:
-    print("It is a tie")
-elif cmp_choice == 0 and user_choice == 2:
-    print("You Lose!")
-elif user_choice == 0 and cmp_choice == 2:
-    print("You Win!")
-elif cmp_choice > user_choice:
-    print("You Lose")
-elif user_choice > cmp_choice:
-    print("You Win!")
+if user_choice >= 3 or user_choice < 1:
+    print("You entered an invalid number, YOU LOSE!")
+else:
+    cmp_choice = random.randint(0,2)
+    print(cmp_choice)
+
+    # Print the conditions in this particular order to ensure game works fine 
+    if cmp_choice == user_choice:
+        print("It is a tie")
+    elif cmp_choice == 0 and user_choice == 2:
+        print("You Lose!")
+    elif user_choice == 0 and cmp_choice == 2:
+        print("You Win!")
+    elif cmp_choice > user_choice:
+        print("You Lose!")
+    elif user_choice > cmp_choice:
+        print("You Win!")
