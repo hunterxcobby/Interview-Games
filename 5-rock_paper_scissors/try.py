339c6638dc7d41449bf0e4e97fa09ccb95f522a1
hunterxcobby <solomonsefah13@gmail.com>
2024-08-27 18:09:28 +0000

    chore: Refactor rock-paper-scissors game code to handle user choices and determine the winner

--- 5-rock_paper_scissors/try.py
+++ 5-rock_paper_scissors/try.py
@@ -4,17 +4,21 @@ import random
 
 user_choice = int(input("Please choose. 0 for Rock , 1 for Paper, 2 for scissors "))
 comp_choice = random.randint(0,2)
-print(comp_choice)
+
 
 
 if user_choice < 1 or user_choice > 2:
     print("You entered an invalid number, YOU LOSE!")
+        
+else:
+    comp_choice = random.randint(0,2)
+    print(f"Computer chose {comp_choice}")
+    
+    if user_choice == comp_choice:
+        print("It is a tie game!")
 
-elif user_choice == comp_choice:
-    print("It is a tie game!")
+    elif user_choice > comp_choice:
+        print("You win !")
 
-elif user_choice > comp_choice:
-    print("You win !")
-
-elif comp_choice > user_choice:
-    print("You Lose!")
+    elif comp_choice > user_choice:
+        print("You Lose!")
