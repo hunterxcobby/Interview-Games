99ac1cccbe2f8194fa5cf4c902fe53b0824c9d5a
hunterxcobby <solomonsefah13@gmail.com>
2024-08-27 17:12:48 +0000

    add

--- 5-rock_paper_scissors/try.py
+++ 5-rock_paper_scissors/try.py
@@ -2,4 +2,12 @@
 
 import random 
 
-user_choice = input("Please choose. 0 for Rock , 1 for Paper, 2 for scissors ")
+user_choice = int(input("Please choose. 0 for Rock , 1 for Paper, 2 for scissors "))
+comp_choice = random.randint(0,2)
+print(comp_choice)
+
+
+if user_choice < 1 or user_choice > 2:
+    print("You entered an invalid number, YOU LOSE!")
+
+
