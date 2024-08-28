12dc07ea898f711f6cffdc181f35ed8072e16a8c
hunterxcobby <solomonsefah13@gmail.com>
2024-08-28 11:23:49 +0000

    Refactor hangman game code to improve modularity and readability, and display chosen word during gameplay

--- 6-hangman/try.py
+++ 6-hangman/try.py
@@ -5,4 +5,24 @@ import random
 word_list = ['beautiful', 'potato', 'apple']
 chosen_word = random.choice(word_list)
 
+
 print(chosen_word)
+
+display = []
+is_running = True
+
+for letter in chosen_word:
+    display += '_'
+
+print(display)
+
+while is_running:
+
+    guessed_letter = input("Type a letter: ").lower()
+
+    for letter in range(len(chosen_word)):
+        if guessed_letter == chosen_word[letter]:
+            print("Match")
+        else:
+            print("No Match")
+    
