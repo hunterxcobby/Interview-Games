6c3db8b258d9b89f517b90778db0e9eae7421bb9
hunterxcobby <solomonsefah13@gmail.com>
2024-08-29 12:32:25 +0000

    Refactor Tic-Tac-Toe game code to add display for X in cell 4

--- 0-tic-tac-toe/try.py
+++ 0-tic-tac-toe/try.py
@@ -8,9 +8,14 @@ class Board():
 
     def display(self):
         print(" %s | %s | %s " %(self.cells[1], self.cells[2], self.cells[3]))
+        print("------------")
+        print(" %s | %s | %s " %(self.cells[4], self.cells[5], self.cells[6]))
+        print("------------")
+        print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
 
 
 
 board = Board()
 
+board.cells[4] = "X"
 board.display()
